02a1d8264a1d65601f98e1ce13c8ba4c381f5d4d
Vishal Keshri <vishalkeshri858@gmail.com>
2024-06-01 13:56:43 +0530

    01-06-2024

--- lib/dialogue/component/dialogue_screen.dart
+++ lib/dialogue/component/dialogue_screen.dart
@@ -15,6 +15,84 @@ class _DialogueBoxScreenState extends State<DialogueBoxScreen> {
         appBar: AppBar(
           elevation: 1,
           shadowColor: Colors.black,
+          leading: const Icon(Icons.menu),
+          centerTitle: true,
+          title: const Text('Dialogue Box'),
+        ),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                OutlinedButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) => const AlertDialog(
+                          title: Text('Alert!!!'),
+                        ),
+                      );
+                    },
+                    child: const Text('Alert')),
+                OutlinedButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: const Text('Are you sure you want to delete the Message'),
+                          actions: [
+                            TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: const Text('Cancel')),
+                            TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: const Text('Ok'))
+                          ],
+                        ),
+                      );
+                    },
+                    child: const Text('Warning')),
+                OutlinedButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: const Text('Enter Pet name for deleting'),
+                          actions: [
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                hintText: 'Pet name',
+                              ),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text('Cancle',style: TextStyle(fontSize: 17),)),
+                                TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text('Done',style: TextStyle(fontSize: 17),)),
+                              ],
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                    child: const Text('Messages')),
+
+              ],
+            )
+          ],
         ),
       ),
     );
